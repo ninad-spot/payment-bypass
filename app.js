8fe44646c74a9075bd825a83dd5e2f12b88f3f7a
Apple <ninad>
2024-10-30 17:14:35 +0530

    pushing

--- app.js
+++ app.js
@@ -7,13 +7,20 @@ const app = express()
 
 app.use(express.json())
 
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const filePath = path.resolve(__dirname, 'paymentStatus.txt');
 
 app.post('/payment/check-status', async (req, res) => {
     const  { amount } = req.body
-    const data = await readFile(filePath, 'utf8');
+    let data;
+    while (true) {
+        data = await readFile(filePath, 'utf8');
+        if (data === 'success') break;
+        await delay(1000);
+    }
     const updatedContent = 'pending';
     await writeFile(filePath, updatedContent, 'utf8');
     return res.json({ message: "Payment Successful" })
@@ -29,4 +36,4 @@ app.post('/payment/pay-now', async (req, res) => {
 
 
 
-app.listen(5090, () => console.log("App running on PORT 5090"))
+app.listen(4090, () => console.log("App running on PORT 5090"))
